b4f0026da72a240e4e7ce5ad502968cdb10549ea
Pingosoft <fernandokulaif@gmail.com>
2021-05-10 19:56:06 -0400

    add the data binding w/ngFor:dashboard ts n html

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -22,15 +22,31 @@ export class DashboardComponent implements OnInit {
   TeamMembersSummary = [];
   TeamMembers = [];
 
+  
+
   ngOnInit() {
     this.Designation = 'Team Leader';
-    this.Username = 'Scott Smith';
-    this.NoOfTeamMembers = 67;
+    this.Username = 'FERNANDO KULAIF';
+    this.NoOfTeamMembers = 4;
     this.TotalCostOfAllProjects = 240;
     this.PendingTasks = 15;
     this.UpComingProjects = 2;
     this.ProjectCost = 2113507;
     this.CurrentExpenditure = 96788;
     this.AvailableFunds = 52536;
+
+    this.Projects=[
+      "Project A","Project B","Project C","Project D"
+    ];
+
+    for(var i=2021; i>=2018; i--) {
+      this.Years.push(i);
+    }
+    
+   
+    // this.Years=[
+    //   2019,2018,2017,2016
+    // ];
+
   }
 }
